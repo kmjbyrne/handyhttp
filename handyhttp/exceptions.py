4f9fd483d3c75736a564ee5c6a3fcecbeb375206
Keith Byrne <keithmbyrne@gmail.com>
2020-08-01 19:05:44 +0100

    initial draft changes

---
+++ handyhttp/exceptions.py
@@ -0,0 +1,32 @@
+
+class HTTPException(Exception):
+    message: str
+    code: int
+
+    def pack(self):
+        return dict(error=self.message), self.code
+
+
+class HTTPClientError(HTTPException):
+    pass
+
+
+class HTTPNotFound(HTTPException):
+    code = 404
+    message = 'Resource does not exist'
+
+
+class HTTPConflict(HTTPException):
+    code = 409
+    message = 'A resource with this ID already exists'
+
+
+class HTTPForbidden(HTTPException):
+    code = 403
+    message = 'You have insufficient permissions to access this resource. If you believe this is invalid, ' \
+              'please contact your system admin.'
+
+
+class HTTPBadRequest(HTTPException):
+    code = 400
+    message = 'This request can not be fulfilled.'
